120b1e3f253f996aaa4776ed255056091df79884
Rhiannon Tully-Barr <rtullybarr@gmail.com>
2021-11-07 13:51:41 -0800

    Creating some initial classes, and a generator for fake EEG data.

---
+++ EEGDataHandling/EEGDataGenerator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+
+namespace EEGDataHandling
+{
+    /// <summary>
+    /// A class for holding mocked EEG data for testing.
+    public class EEGDataGenerator
+    {
+        /// <summary>
+        /// Generator for time-series EEG data
+        /// For simplicity (for now), timestamps are in milliseconds from
+        /// 'start' and values are in the range (0, 1).
+        /// </summary>
+        public EEGDataGenerator()
+        {
+            dataPoints = new List<(long, double)>();
+            random = new Random();
+            timer = new Timer();
+
+            timer.Interval = 25;
+        }
+
+        /// <summary>
+        /// Determines the interval at which this MockEEGData class will 
+        /// generate data.
+        /// Changing this has no effect if the timer is running.
+        /// </summary>
+        public double Interval
+        {
+            get => timer.Interval;
+            set => timer.Interval = value;
+        }
+
+        private List<(long, double)> dataPoints;
+        public IEnumerable<(long, double)> DataPoints => dataPoints;
+
+        private Timer timer;
+        private Random random;
+
+        /// <summary>
+        /// Not part of the interface.
+        /// Call to create a timer that adds a (random) data point to DataPoints
+        /// every Interval milliseconds.
+        /// </summary>
+        public void StartGenerating()
+        {
+            timer.Interval = Interval;
+
+            timer.AutoReset = true;
+            timer.Elapsed += (sender, e) => GenerateDataPoint(e.SignalTime);
+
+            timer.Start();
+        }
+
+        /// <summary>
+        ///  Call to stop generating data.
+        /// </summary>
+        public void StopGenerating()
+        {
+            timer.Stop();
+        }
+
+        public void GenerateDataPoint(DateTime time)
+        {
+            DateTimeOffset offset = time;
+            double voltage = random.NextDouble();
+            long timeMs = offset.ToUnixTimeMilliseconds();
+
+            AddDataPoint(timeMs, voltage);
+        }
+
+        public void AddDataPoint(long timeMs, double voltage)
+        {
+            dataPoints.Add((timeMs, voltage));
+        }
+    }
+}
---
+++ EEGMachine/ViewModels/WaveformViewModel.cs
@@ -0,0 +1,18 @@
+﻿using EEGDataHandling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EEGMachine.ViewModels
+{
+    public class WaveformViewModel
+    {
+        public WaveformViewModel(IEEGData data)
+        {
+            Data = data;
+        }
+
+        // Obviously this doesn't work for live updates
+        public IEEGData Data { get; }
+    }
+}
