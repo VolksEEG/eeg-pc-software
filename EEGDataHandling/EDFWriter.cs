42308673c3fd7ae5f53c3accc4a075b42f740daf
Rhiannon Tully-Barr <rtullybarr@gmail.com>
2021-11-20 21:38:57 -0800

    Adding some EDF file handling.
    Imported an EDF file reading and writing library, and started working on a test for it.

---
+++ EEGDataHandling/EDFWriter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using SharpEDF = SharpLib.EuropeanDataFormat;
+
+namespace EEGDataHandling
+{
+    public class EDFWriter
+    {
+        // Construct with the appropriate metadata.
+        public EDFWriter(EEGRecordingMetadata recordingMetadata, IList<IEEGData> channelData)
+        {
+            _headerData = recordingMetadata;
+            _channelData = channelData;
+            
+        }
+
+        private EEGRecordingMetadata _headerData;
+        private IList<IEEGData> _channelData;
+
+        public void Write(string path)
+        {
+            // Create an EDF File
+            SharpEDF.File edfFile = new SharpEDF.File();
+
+            // Set header properties from metadata.
+            SharpEDF.Header header = _headerData.ToEDFHeader();
+
+            header.SignalCount.Value = _channelData.Count;
+            header.Signals.Reserveds.Value = Enumerable.Repeat("RESERVED", header.SignalCount.Value).ToArray();
+
+            // Record count: since RecordDurationInSeconds is 1s, this is
+            // equivalent to the number of seconds in the data we're writing.
+
+            // Get longest duration?
+            // (there are some things that are awkward / problematic about this).
+            // For EDF export, we must have a very consistent sampling rate.
+            long longestDuration = 0;
+            foreach(IEEGData channel in _channelData)
+            {
+                long min = channel.DataPoints.Select(x => x.timestamp).Min();
+                long max = channel.DataPoints.Select(x => x.timestamp).Max();
+                long duration = max - min;
+
+                if (longestDuration < duration)
+                {
+                    longestDuration = duration;
+                }
+            }
+
+            // Number of seconds in the data. (round up).
+            header.RecordCount.Value = (int)Math.Ceiling(longestDuration / (_headerData.RecordDurationInSeconds * 1000.0));
+
+            edfFile.Header = header;
+
+            edfFile.Signals = _channelData.Select(x => x.ToEDFSignal(_headerData.RecordDurationInSeconds)).ToArray();
+
+            edfFile.Save(path);
+        }
+    }
+}
---
+++ EEGMachineTests/EDFWriterTest.cs
@@ -0,0 +1,106 @@
+﻿using EEGDataHandling;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using EDFFile = SharpLib.EuropeanDataFormat.File;
+
+namespace EEGMachineTests
+{
+    public class EDFWriterTest
+    {
+        // Write an EDF File.
+        [Test]
+        public void TestWriteEDFFile()
+        {
+            // Using a specific date time offset for ease of verification.
+            var startTime = DateTimeOffset.Parse("2021-11-19 06:09:00Z");
+
+            // Initialize metadata
+            EEGRecordingMetadata metadata = new EEGRecordingMetadata()
+            {
+                PatientID = "Patient ID",
+                RecordingID = "Recording ID",
+                RecordDurationInSeconds = 1,
+                StartTime = startTime
+            };
+
+            // Initialize signal metadata for one signal
+            EEGSignalMetadata signalMetadata = new EEGSignalMetadata()
+            {
+                Label = "Label",
+                Units = "Units",
+                TransducerType = "Transducer type",
+                Prefiltering = "Prefiltering"
+            };
+
+            // Mock IEEGData
+            var mock = new Mock<IEEGData>();
+
+            // Return our signalMetadata
+            mock.Setup(x => x.SignalMetadata).Returns(signalMetadata);
+
+            // Mock other relevant properties
+            mock.Setup(x => x.PhysicalMinimum).Returns(-20);
+            mock.Setup(x => x.PhysicalMaximum).Returns(20);
+            mock.Setup(x => x.DigitalMinimum).Returns(0);
+            mock.Setup(x => x.DigitalMaximum).Returns(20);
+
+            // Mock sample data - 2 samples/second (total of 20 samples.)
+            long startTimeMs = startTime.ToUnixTimeMilliseconds();
+
+            var dataPoints = new List<(long timestamp, double value)>();
+            for (int i = 0; i < 20; i++)
+            {
+                dataPoints.Add((startTimeMs + i*500, i));
+            }
+
+            mock.Setup(x => x.DataPoints).Returns(dataPoints);
+
+            EDFWriter writer = new EDFWriter(metadata, new List<IEEGData>() { mock.Object });
+
+            var edfFileName = "test.edf";
+
+            writer.Write(edfFileName);
+
+            // Read file as string.
+            string fileContent = File.ReadAllText(edfFileName);
+            Console.WriteLine(fileContent);
+
+            // Read file as EDF
+            var edf2 = new EDFFile(edfFileName);
+
+            Assert.AreEqual("0", edf2.Header.Version.Value, "Version mismatch");
+            Assert.AreEqual(metadata.PatientID, edf2.Header.PatientID.Value, "PatientID mismatch");
+            Assert.AreEqual(metadata.RecordingID, edf2.Header.RecordID.Value, "RecordID mismatch");
+            Assert.AreEqual(metadata.StartTime.ToString("dd.MM.yy"), edf2.Header.RecordingStartDate.Value, "RecordStartDate mismatch");
+            Assert.AreEqual(metadata.StartTime.ToString("hh.mm.ss"), edf2.Header.RecordingStartTime.Value, "RecordStartTime mismatch");
+            Assert.AreEqual(10, edf2.Header.RecordCount.Value, "RecordCount mismatch");
+            Assert.AreEqual(1, edf2.Header.SignalCount.Value, "SignalCount mismatch");
+
+            Assert.AreEqual("RESERVED", edf2.Header.Reserved.Value, $"Header Reserved mismatch");
+
+            Assert.AreEqual(1, edf2.Signals.Length, "Signals Length mismatch");
+
+            for (int i = 0; i < edf2.Signals.Length; i++)
+            {
+                Assert.AreEqual(2, edf2.Signals[i].SampleCountPerRecord.Value, $"Signal {i} SampleCountPerRecord mismatch");
+                Assert.AreEqual(signalMetadata.Units,  edf2.Signals[i].PhysicalDimension.Value, $"Signal {i} PhysicalDimension mismatch");
+                Assert.AreEqual(signalMetadata.Prefiltering, edf2.Signals[i].Prefiltering.Value, $"Signal {i} Prefiltering mismatch");
+                Assert.AreEqual(signalMetadata.TransducerType, edf2.Signals[i].TransducerType.Value, $"Signal {i} TransducerType mismatch");
+                Assert.AreEqual(mock.Object.PhysicalMinimum, edf2.Signals[i].PhysicalMinimum.Value, $"Signal {i} PhysicalMinimum mismatch");
+                Assert.AreEqual(mock.Object.PhysicalMaximum, edf2.Signals[i].PhysicalMaximum.Value, $"Signal {i} PhysicalMaximum mismatch");
+
+                Assert.AreEqual("RESERVED", edf2.Signals[i].Reserved.Value, $"Signal {i} Reserved mismatch");
+
+                CollectionAssert.AreEqual(mock.Object.DataPoints.Select(x => x.value), edf2.Signals[i].Samples);
+            }
+
+            // clean up the file.
+            File.Delete(edfFileName);
+        }
+    }
+}
