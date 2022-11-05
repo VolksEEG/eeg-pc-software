143b6aa036033931aae95377be48e775c9c12c75
alcohen <amc79@cornell.edu>
2022-11-05 11:21:58 -0400

    Added Simple Packet Receiver back in as part of this repo - not pointing to another repo

---
+++ Simple Packet Receiver/frmMain.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace Simple_Packet_Receiver
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAcquire = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.tmrUpdateDisplay = new System.Windows.Forms.Timer(this.components);
+            this.btnCopyClipboard = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnClearDisplay = new System.Windows.Forms.Button();
+            this.updBufferLength = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.updBufferLength)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAcquire
+            // 
+            this.btnAcquire.BackColor = System.Drawing.SystemColors.Control;
+            this.btnAcquire.Enabled = false;
+            this.btnAcquire.Location = new System.Drawing.Point(12, 22);
+            this.btnAcquire.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnAcquire.Name = "btnAcquire";
+            this.btnAcquire.Size = new System.Drawing.Size(130, 48);
+            this.btnAcquire.TabIndex = 0;
+            this.btnAcquire.Text = "Acquire";
+            this.btnAcquire.UseVisualStyleBackColor = false;
+            this.btnAcquire.Click += new System.EventHandler(this.BtnAcquire_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(341, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Port";
+            // 
+            // cmbPort
+            // 
+            this.cmbPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(395, 28);
+            this.cmbPort.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(165, 38);
+            this.cmbPort.TabIndex = 3;
+            this.cmbPort.SelectedIndexChanged += new System.EventHandler(this.CmbPort_SelectedIndexChanged);
+            // 
+            // tmrUpdateDisplay
+            // 
+            this.tmrUpdateDisplay.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // btnCopyClipboard
+            // 
+            this.btnCopyClipboard.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCopyClipboard.Location = new System.Drawing.Point(195, 492);
+            this.btnCopyClipboard.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnCopyClipboard.Name = "btnCopyClipboard";
+            this.btnCopyClipboard.Size = new System.Drawing.Size(194, 50);
+            this.btnCopyClipboard.TabIndex = 4;
+            this.btnCopyClipboard.Text = "Copy to Clipboard";
+            this.btnCopyClipboard.UseVisualStyleBackColor = true;
+            this.btnCopyClipboard.Click += new System.EventHandler(this.BtnCopyClipboard_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.BackColor = System.Drawing.Color.Black;
+            this.txtOutput.ForeColor = System.Drawing.Color.LimeGreen;
+            this.txtOutput.Location = new System.Drawing.Point(12, 84);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(1576, 396);
+            this.txtOutput.TabIndex = 5;
+            this.txtOutput.Text = "";
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.SystemColors.Control;
+            this.btnPause.Enabled = false;
+            this.btnPause.Location = new System.Drawing.Point(149, 22);
+            this.btnPause.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(130, 48);
+            this.btnPause.TabIndex = 6;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = false;
+            this.btnPause.Click += new System.EventHandler(this.BtnPause_Click);
+            // 
+            // btnClearDisplay
+            // 
+            this.btnClearDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnClearDisplay.Location = new System.Drawing.Point(12, 492);
+            this.btnClearDisplay.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnClearDisplay.Name = "btnClearDisplay";
+            this.btnClearDisplay.Size = new System.Drawing.Size(177, 50);
+            this.btnClearDisplay.TabIndex = 7;
+            this.btnClearDisplay.Text = "Clear Display";
+            this.btnClearDisplay.UseVisualStyleBackColor = true;
+            this.btnClearDisplay.Click += new System.EventHandler(this.BtnClearDisplay_Click);
+            // 
+            // updBufferLength
+            // 
+            this.updBufferLength.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.updBufferLength.Location = new System.Drawing.Point(586, 498);
+            this.updBufferLength.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.updBufferLength.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.updBufferLength.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.updBufferLength.Name = "updBufferLength";
+            this.updBufferLength.Size = new System.Drawing.Size(142, 35);
+            this.updBufferLength.TabIndex = 8;
+            this.updBufferLength.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.updBufferLength.ThousandsSeparator = true;
+            this.updBufferLength.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.updBufferLength.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(473, 502);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 30);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Buffer Size";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(735, 500);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 30);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "(# lines)";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1599, 554);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.updBufferLength);
+            this.Controls.Add(this.btnClearDisplay);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnCopyClipboard);
+            this.Controls.Add(this.cmbPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAcquire);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmMain";
+            this.Text = "SImple Packet Receiver";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.FrmMain_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.updBufferLength)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAcquire;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.Windows.Forms.Timer tmrUpdateDisplay;
+        private System.Windows.Forms.Button btnCopyClipboard;
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnClearDisplay;
+        private System.Windows.Forms.NumericUpDown updBufferLength;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Simple Packet Receiver/frmMain.cs
@@ -0,0 +1,222 @@
+﻿// <copyright file="frmMain.cs" company="VolksEEG Project">
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// </copyright>
+
+namespace Simple_Packet_Receiver
+{
+    using System;
+    using System.Diagnostics;
+    using System.Drawing;
+    using System.IO.Ports;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Forms;
+    using CircularBuffer;
+    using SimplePacketLib;
+
+    public partial class FrmMain : Form
+    {
+        private SerialPort serialPort = new SerialPort();
+        private int numPacketsReceived = 0;
+        private SimplePacketParser packetizer = new SimplePacketParser(21);
+        private SimplePacket packetBuffer = new SimplePacket();
+        private Stopwatch stopwatch = new Stopwatch();
+        private string prevDispString = string.Empty;
+        private object mylock = new object();
+        private CircularBuffer<string> outLinesBuffer;
+        private string[] outLines = new string[1000];
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FrmMain"/> class.
+        /// </summary>
+        public FrmMain()
+        {
+            this.InitializeComponent();
+        }
+
+        private delegate void SetTextCallback(string text);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // set form location and size
+            if (Properties.Settings.Default.frmMainLocation == new Point(0, 0))
+            {
+                FormHelpers.Centerform(this);
+            }
+            else if ((Properties.Settings.Default.frmMainLocation.X
+                + Properties.Settings.Default.frmMainSize.Width)
+                >= Screen.PrimaryScreen.Bounds.Width)
+            {
+                FormHelpers.Centerform(this);
+            }
+            else
+            {
+                this.Location = Properties.Settings.Default.frmMainLocation;
+                this.Size = Properties.Settings.Default.frmMainSize;
+            }
+
+            // register serial port event handler
+            this.serialPort.DataReceived += new SerialDataReceivedEventHandler(this.SerialReceivedHandler);
+
+            // populate port dropdown with this computer's com ports
+            var ports = SerialPort.GetPortNames();
+            this.cmbPort.DataSource = ports;
+
+            // if com port saved in configuration exists on computer, then set to that
+            if (ports.Contains<string>(Properties.Settings.Default.SelectedPort))
+            {
+                this.cmbPort.Text = Properties.Settings.Default.SelectedPort;
+            }
+        }
+
+        private void BtnAcquire_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.serialPort.PortName = this.cmbPort.Text;
+                Properties.Settings.Default.SelectedPort = this.cmbPort.Text;
+                this.serialPort.BaudRate = 921600;
+                this.serialPort.Parity = Parity.None;
+                this.serialPort.DataBits = 8;
+                this.serialPort.StopBits = StopBits.One;
+                this.serialPort.Open();
+                this.tmrUpdateDisplay.Enabled = true;
+                this.stopwatch.Start();
+                this.outLinesBuffer = new CircularBuffer<string>((int)this.updBufferLength.Value);
+                this.ChangeToAcquiringState();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SerialReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            for (int i = 0; i < sp.BytesToRead; i++)
+            {
+                byte b = Convert.ToByte(sp.ReadByte());
+                bool newPacket = this.packetizer.AddByte(b, this.packetBuffer);
+                if (newPacket)
+                {
+                    StringBuilder lineToPrint = new StringBuilder();
+                    lineToPrint.Append(Environment.NewLine);
+                    lineToPrint.Append(this.packetBuffer.StartFlag + "\t" + this.packetBuffer.Counter);
+                    for (int j = 0; j < this.packetBuffer.ChannelSet.Length; j++)
+                    {
+                        lineToPrint.Append("\t" + this.packetBuffer.ChannelSet[j].ToString());
+                    }
+
+                    this.outLinesBuffer.PushBack(lineToPrint.ToString());
+                    this.numPacketsReceived++;
+                }
+            }
+        }
+
+        private void SetText(string text)
+        {
+            this.txtOutput.AppendText(text);
+            Application.DoEvents();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            this.DisplayPackets();
+        }
+
+        private void DisplayPackets()
+        {
+            lock (this.mylock)
+            {
+                this.txtOutput.Text = this.GetOutText();
+                if (this.txtOutput.Text != this.prevDispString)
+                {
+                    this.txtOutput.SelectionStart = this.txtOutput.Text.Length;
+                    this.txtOutput.ScrollToCaret();
+                    this.prevDispString = this.txtOutput.Text;
+                }
+            }
+        }
+
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.frmMainLocation = this.Location;
+            Properties.Settings.Default.frmMainSize = this.Size;
+            Properties.Settings.Default.SelectedPort = this.cmbPort.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void FrmMain_Resize(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.frmMainSize = this.Size;
+        }
+
+        private void BtnCopyClipboard_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(this.txtOutput.Text);
+        }
+
+        private string GetOutText()
+        {
+            StringBuilder outText = new StringBuilder();
+            this.outLines = this.outLinesBuffer.ToArray<string>();
+            for (int i = 0; i < this.outLines.Length; i++)
+            {
+                if (this.outLines[i] != string.Empty)
+                {
+                    outText.Append(this.outLines[i]);
+                }
+            }
+
+            return outText.ToString();
+        }
+
+        private void ChangeToPausedState()
+        {
+            this.btnAcquire.Enabled = true;
+            this.btnPause.Enabled = false;
+            this.btnClearDisplay.Enabled = true;
+            this.btnCopyClipboard.Enabled = true;
+            this.cmbPort.Enabled = true;
+            this.updBufferLength.Enabled = true;
+            this.serialPort.Close();
+        }
+
+        private void ChangeToAcquiringState()
+        {
+            this.btnAcquire.Enabled = false;
+            this.btnPause.Enabled = true;
+            this.btnClearDisplay.Enabled = true;
+            this.btnCopyClipboard.Enabled = true;
+            this.cmbPort.Enabled = false;
+            this.updBufferLength.Enabled = false;
+        }
+
+        private void CmbPort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.ChangeToPausedState();
+        }
+
+        private void BtnClearDisplay_Click(object sender, EventArgs e)
+        {
+            this.outLinesBuffer.Clear();
+            this.DisplayPackets();
+        }
+
+        private void BtnPause_Click(object sender, EventArgs e)
+        {
+            this.ChangeToPausedState();
+        }
+    }
+}
